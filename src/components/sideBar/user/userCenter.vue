73f5b1d88397701b25f28ee0d05b58ea0d19dd3e
Michael <tioni811@gmail.com>
2025-04-09 16:44:49 +0800

    使用者區塊更新-信箱和使用者名稱

--- src/components/sideBar/user/userCenter.vue
+++ src/components/sideBar/user/userCenter.vue
@@ -2,6 +2,13 @@
     <div class="userCenterBolck">
         <form @submit.prevent="updateUserInfo">
             <div class="userCenterDataBlock">
+                <p class="inputTitle">Name</p>
+                <div class="flexWrap">
+                    <input type="text" v-model="nameText" autocomplete="name" required class="input inputWithBtn"
+                        :readonly="!isChangeName" @click="!isChangeName && notify('名稱請按旁邊EDIT按鈕修改', 'info')" />
+                    <p class="inputsBtn flexAllCenter" @click="changeName" :class="{ btnActive: isChangeName }">
+                        {{ isChangeName ? 'SAVE' : 'EDIT' }}</p>
+                </div>
                 <p class="inputTitle">Account</p>
                 <input type="text" v-model="accText" autocomplete="username" required class="input" readonly
                     @click="notify('帳戶無法修改', 'error')" />
@@ -25,10 +32,12 @@
                 </transition>
                 <p class="inputTitle">E-mail</p>
                 <div class="flexWrap">
-                    <input type="email" v-model="regMailText" autocomplete="email" required class="input inputWithBtn"
-                        :readonly="!isChangeEmail" @click="!isChangeEmail && notify('信箱請按旁邊EDIT按鈕修改', 'info')" />
-                    <p class="inputsBtn flexAllCenter" @click="changeEmail" :class="{ btnActive: isChangeEmail }">{{
-                        isChangeEmail ? 'SAVE' : 'EDIT' }}</p>
+                    <input type="email" v-model="mailText" autocomplete="email" required class="input inputWithBtn"
+                        :readonly="!isChangeEmail"
+                        @click="!isChangeEmail && notify(isChangeEmail ? '信箱請按旁邊EDIT按鈕修改' : '請先驗證信箱', isChangeEmail ? 'info' : 'error')" />
+                    <p class="inputsBtn flexAllCenter" @click="changeEmail"
+                        :class="{ btnActive: isChangeEmail, isEmailVerify: !isEmailVerify }">
+                        {{ changeEmailBtn }}</p>
                 </div>
             </div>
 
@@ -39,38 +48,82 @@
     </div>
 </template>
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
+const nameText = ref('');
 const accText = ref('');
-const pwdText = ref('');
+const pwdText = ref('************');
 const regPwdText = ref('');
 const regPwdAgainText = ref('');
-const regMailText = ref('');
+const mailText = ref('');
+const isChangeName = ref(false);
 const isChangePwd = ref(false);
 const isChangeEmail = ref(false);
+const isEmailVerify = ref(true);
 const notify = window.notify;
+const changeEmailBtn = ref('EDIT');
 
+// 模擬使用者資料
+const userData = reactive({
+    userId: 1,
+    account: 'user123',
+    email: 'email@email.com',
+    isEmailVerify: true,
+});
+
+accText.value = userData.account;
+mailText.value = userData.email;
+isEmailVerify.value = userData.isEmailVerify;
+
+const changeName = () => {
+    isChangeName.value = !isChangeName.value;
+
+}
 
 const changePwd = () => {
     isChangePwd.value = !isChangePwd.value;
     if (isChangePwd.value) {
-        pwdText.value = "";
+        regPwdText.value = '';
+        regPwdAgainText.value = '';
     } else {
-        pwdText.value = "********";
-        window.notify('修改成功', 'success');
-
+        if (regPwdText.value === '' && regPwdAgainText.value === '') {
+            notify('密碼未變更', 'info');
+        } else if (regPwdText.value === '') {
+            notify('修改失敗，請輸入新密碼', 'error');
+        } else if (regPwdAgainText.value === '') {
+            notify('修改失敗，請再次輸入新密碼', 'error');
+        } else if (regPwdText.value !== regPwdAgainText.value) {
+            notify('修改失敗，兩次密碼不相同', 'error');
+        } else {
+            pwdText.value = regPwdText.value;
+            window.notify('修改成功', 'success');
+        }
     }
 }
 
 const changeEmail = () => {
-    isChangeEmail.value = !isChangeEmail.value;
-    if (isChangeEmail.value) {
-        regMailText.value = "";
-    } else {
-        regMailText.value = "********@example.com";
-        window.notify('修改成功', 'success');
+    if (!isEmailVerify.value && !isChangeEmail.value) {
+        notify('信箱尚未驗證，無法再次修改', 'error');
+        return;
     }
-}
+
+    isChangeEmail.value = !isChangeEmail.value;
+
+    if (!isChangeEmail.value) {
+        if (mailText.value !== userData.email) {
+            notify('修改成功，請到新信箱進行驗證', 'success');
+            isEmailVerify.value = false;
+            userData.email = mailText.value;
+            changeEmailBtn.value = 'WAIT';
+        } else {
+            notify('信箱未變更', 'info');
+            changeEmailBtn.value = 'EDIT';
+        }
+    } else {
+        changeEmailBtn.value = 'SAVE';
+    }
+};
+
 
 const updateUserInfo = () => {
     console.log(accText.value);
@@ -204,6 +257,10 @@ const updateUserInfo = () => {
     box-sizing: border-box;
     color: var(--gray);
     font-weight: 700;
+}
+
+.isEmailVerify {
+    background-color: var(--removeRed);
 }
 
 /* 動畫：向下滑入與滑出 */
